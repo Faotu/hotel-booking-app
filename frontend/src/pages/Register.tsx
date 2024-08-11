32f9057bd33cdfbd5667f4aa7c71e1c004cc59c2
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-08-11 19:09:25 +0100

    registration validation

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -9,21 +9,96 @@ type RegisterFormData = {
 };
 
 const Register = () => {
-  const { register } = useForm<RegisterFormData>();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
   return (
-    <div className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5 pl-10" onSubmit={onSubmit}>
       <h1 className="text-3xl font-bold">Create an Account</h1>
       <div className="flex flex-col md:flex-row gap-5 ">
         <label className="text-gray-700 text-sm font-bold flex-1">
           First Name{" "}
-          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "First Name is required" })}
+          ></input>
+          {errors.firstName && (
+            <span className="text-red-600">{errors.firstName.message}</span>
+          )}
         </label>
         <label className="text-gray-700 text-sm font-bold flex-1">
           Last Name{" "}
-          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "Surname is required" })}
+          ></input>
+          {errors.lastName && (
+            <span className="text-red-600">{errors.lastName.message}</span>
+          )}
         </label>
       </div>
-    </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email{" "}
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "Your email is required" })}
+        ></input>
+        {errors.email && (
+          <span className="text-red-600">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password{" "}
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "password must be at least 6 characters",
+            },
+          })}
+        ></input>
+        {errors.password && (
+          <span className="text-red-600">{errors.password.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password{" "}
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Password do not match";
+              }
+            },
+          })}
+        ></input>
+        {errors.confirmPassword && (
+          <span className="text-red-600">{errors.confirmPassword.message}</span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-green-700 text-white p-2 font-bold hover:bg-green-600 text-xl "
+        >
+          Create Account
+        </button>
+      </span>
+    </form>
   );
 };
 
