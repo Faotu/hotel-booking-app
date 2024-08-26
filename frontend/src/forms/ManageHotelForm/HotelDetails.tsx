8beceb6528e609c2da02926c067312c9c519c2a4
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-08-26 18:08:29 +0100

    add hotel

---
+++ frontend/src/forms/ManageHotelForm/HotelDetails.tsx
@@ -0,0 +1,78 @@
+import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
+
+const HotelDetails = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-3xl font-bold mb-3">Add an Hotel</h1>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Name{" "}
+        <input
+          type="text"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("name", { required: "Your email is required" })}
+        ></input>
+        {errors.name && (
+          <span className="text-red-600">{errors.name.message}</span>
+        )}
+      </label>
+      <div className="flex gap-4">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          City{" "}
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("city", { required: "city is required" })}
+          ></input>
+          {errors.city && (
+            <span className="text-red-600">{errors.city.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Country{" "}
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("country", { required: "Country is required" })}
+          ></input>
+          {errors.country && (
+            <span className="text-red-600">{errors.country.message}</span>
+          )}
+        </label>
+      </div>
+
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Description{" "}
+        <textarea
+          rows={10}
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("description", { required: "Description is required" })}
+        ></textarea>
+        {errors.description && (
+          <span className="text-red-600">{errors.description.message}</span>
+        )}
+      </label>
+
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Price per Night{" "}
+        <input
+          type="number"
+          min={1}
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("pricePerNight", {
+            required: "hotel price is required",
+          })}
+        ></input>
+        {errors.pricePerNight && (
+          <span className="text-red-600">{errors.pricePerNight.message}</span>
+        )}
+      </label>
+    </div>
+  );
+};
+
+export default HotelDetails;
--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,5 +1,30 @@
+import { FormProvider, useForm } from "react-hook-form";
+import HotelDetails from "./HotelDetails";
+
+export type HotelFormData = {
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  starRating: number;
+  facilities: string[];
+  imageFile: FileList;
+  adultCount: number;
+  childCount: number;
+};
 const ManageHotelForm = () => {
-  return <form></form>;
+  const formMethods = useForm<HotelFormData>();
+
+  return (
+    <FormProvider {...formMethods}>
+      {" "}
+      <form>
+        <HotelDetails />
+      </form>
+    </FormProvider>
+  );
 };
 
 export default ManageHotelForm;
