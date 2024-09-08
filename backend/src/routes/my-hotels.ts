01c1ef64a7e9bc7835720bd2f6d926afac44bfca
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-09-08 09:05:10 +0100

    adding

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -70,7 +70,20 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
     const hotels = await Hotel.find({ userId: req.userId });
     res.json(hotels);
   } catch (error) {
-    res.status(500).json({ message: "Error fetching hotels!" });
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+  const id = req.params.id.toString();
+  try {
+    const hotel = await Hotel.findOne({
+      _id: id,
+      userId: req.userId,
+    });
+    res.json(hotel);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
   }
 });
 
--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -23,7 +23,7 @@ const MyHotels = () => {
         <h1 className="text-3xl font-bold">My Hotels</h1>
         <Link
           to="/add-hotel"
-          className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
+          className="flex bg-green-600 text-white text-xl font-bold p-2 hover:bg-green-500"
         >
           Add Hotel
         </Link>
